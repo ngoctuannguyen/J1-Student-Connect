5c24b528c124eb41f30440d1112fc533f088e5b3
ngoctuannguyen <ngoctuannguyen1980123@gmail.com>
2023-07-13 11:27:39 +0700

    Update delete Calendar and Notifications in Home

--- app/src/main/java/com/example/j1studentconnect/MainActivity.java
+++ app/src/main/java/com/example/j1studentconnect/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.j1studentconnect;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -11,13 +12,16 @@ import android.widget.ImageButton;
 public class MainActivity extends AppCompatActivity {
 
     private ImageButton btnSearch, btnProfile, btnCalendar,
-                        btnCalendarHotkey, btnAvatar;
+                        btnCalendarHotkey, btnAvatar, btnX;
+
+    CardView ConvenientCard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ConstructButton();
         ClickButton();
+        ConstructLayout();
     }
 
     private void ConstructButton(){
@@ -27,6 +31,13 @@ public class MainActivity extends AppCompatActivity {
         btnCalendar = (ImageButton) findViewById(R.id.TimeTable);
         btnCalendarHotkey = (ImageButton) findViewById(R.id.calendarHotKey);
         btnAvatar = (ImageButton) findViewById(R.id.dogAvt);
+        btnX = (ImageButton) findViewById(R.id.x);
+
+    }
+
+    private void ConstructLayout(){
+
+        ConvenientCard = (CardView) findViewById(R.id.convenientNoti);
 
     }
 
@@ -63,6 +74,14 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 startActivity(new Intent(MainActivity.this, CalendarActivity.class));
+            }
+        });
+
+        btnX.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ConvenientCard.setVisibility(View.GONE);
+
             }
         });
 
