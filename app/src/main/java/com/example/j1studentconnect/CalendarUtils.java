3e380618aad365e471a6c4a37d9b0ae19a3e5a7b
ngoctuannguyen <ngoctuannguyen1980123@gmail.com>
2023-07-17 22:54:52 +0700

    Create CalendarAdapter.java + CalendarViewHolder.java + Debug Login.java

--- app/src/main/java/com/example/j1studentconnect/CalendarUtils.java
+++ app/src/main/java/com/example/j1studentconnect/CalendarUtils.java
@@ -6,7 +6,6 @@ import androidx.annotation.RequiresApi;
 
 import java.time.DayOfWeek;
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.time.YearMonth;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
