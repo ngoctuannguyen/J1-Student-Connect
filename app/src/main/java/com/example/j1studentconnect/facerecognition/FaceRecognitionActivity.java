6610677671adf64f6a5aec26e2e6a67c687293f0
ngoctuannguyen <ngoctuannguyen1980123@gmail.com>
2023-08-04 23:47:48 +0700

    Litte change ...

--- app/src/main/java/com/example/j1studentconnect/facerecognition/FaceRecognitionActivity.java
+++ app/src/main/java/com/example/j1studentconnect/facerecognition/FaceRecognitionActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Rect;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -37,6 +38,11 @@ public class FaceRecognitionActivity extends AppCompatActivity {
 
     private FaceDetector detector;
 
+    private static boolean img1 = true;
+    //Bitmap cropBitmap2 = null, cropBitmap1 = null;
+
+    double chiSquare = 0.0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -61,6 +67,8 @@ public class FaceRecognitionActivity extends AppCompatActivity {
             public void onClick(View view) {
                 //from drawable
                 Bitmap bitmap = BitmapFactory.decodeResource(getResources(), R.drawable.thay_trinh);
+                Bitmap bitmap1 = BitmapFactory.decodeResource(getResources(), R.drawable.thay_trinh_2);
+                //bitmap1 = BitmapFactory.decodeResource(getResources(), R.drawable.thay_trinh_2);
 
                 //Bitmap from Uri, in case to detect face from mage picked from gallery
 //                Uri imageUri = null;
@@ -74,22 +82,33 @@ public class FaceRecognitionActivity extends AppCompatActivity {
 //                BitmapDrawable bitmapDrawable = (BitmapDrawable) originalImageIv.getDrawable();
 //                Bitmap bitmap1 = bitmapDrawable.getBitmap();
 
+                //Bitmap cropBitmap1 = null, cropBitmap2 = null;
+
                 analyzePhoto(bitmap);
+                //img1 = false;
+                analyzePhoto(bitmap1);
+                //img1 = true;
+
+                Bitmap cropbitmap1, cropbitmap2;
+                cropbitmap1 = ((BitmapDrawable)originalImageIv.getDrawable()).getBitmap();
+                cropbitmap2 = ((BitmapDrawable)croppedImageIv.getDrawable()).getBitmap();
+                HistogramCalculate(cropbitmap1, cropbitmap2);
             }
 
 
         });
     }
 
-    private void analyzePhoto(Bitmap bitmap){
+
+    private void analyzePhoto(Bitmap bitmap) {
 
         Log.d(TAG, "analyzePhoto");
 
         //Get smaller Bitmap to do analyze process faster
         Bitmap smallerBitmap = Bitmap.createScaledBitmap(
                 bitmap,
-                bitmap.getWidth()/SCALING_FACTOR,
-                bitmap.getHeight()/SCALING_FACTOR,
+                bitmap.getWidth() / SCALING_FACTOR,
+                bitmap.getHeight() / SCALING_FACTOR,
                 false
         );
 
@@ -103,13 +122,13 @@ public class FaceRecognitionActivity extends AppCompatActivity {
                     public void onSuccess(List<Face> faces) {
                         //can be multiple faces detected from an image, manage them using loop from List<Face> faces
                         Log.d(TAG, "on Success: No of faces detected" + faces.size());
-                        for (Face face : faces){
+                        for (Face face : faces) {
                             //Get detected face as rectangle
                             Rect rect = face.getBoundingBox();
-                            rect.set(rect.left*SCALING_FACTOR,
-                                    rect.top*(SCALING_FACTOR-1),
-                                    rect.right*SCALING_FACTOR,
-                                    rect.bottom*SCALING_FACTOR + 90);
+                            rect.set(rect.left * SCALING_FACTOR,
+                                    rect.top * (SCALING_FACTOR - 1),
+                                    rect.right * SCALING_FACTOR,
+                                    rect.bottom * SCALING_FACTOR + 90);
                         }
                         cropDetectedFaces(bitmap, faces);
                     }
@@ -119,11 +138,12 @@ public class FaceRecognitionActivity extends AppCompatActivity {
                     public void onFailure(@NonNull Exception e) {
                         //Detection failed
                         Log.e(TAG, "onFailed ", e);
-                        Toast.makeText(FaceRecognitionActivity.this, "Detection failed due to"+ e.getMessage(), Toast.LENGTH_SHORT);
+                        Toast.makeText(FaceRecognitionActivity.this, "Detection failed due to" + e.getMessage(), Toast.LENGTH_SHORT);
                     }
                 });
 
     }
+
 
     private void cropDetectedFaces(Bitmap bitmap, List<Face> faces) {
 
@@ -146,37 +166,127 @@ public class FaceRecognitionActivity extends AppCompatActivity {
         );
 
         //set the cropped bitmap to imageview
-        croppedImageIv.setImageBitmap(croppedBitmap);
-        HistogramComputing histogramComputing = new HistogramComputing();
-        int[] histogram1 = histogramComputing.HistogramComputing(croppedBitmap);
-        int[] histogram2 = histogramComputing.HistogramComputing(bitmap);
-        int totalObservations = 0;
-        for (int i = 0; i < histogram1.length; i++){
-            totalObservations += histogram1[i];
-            totalObservations += histogram2[i];
+        //Bitmap cropBitmap1, cropBitmap2;
+        if (img1) {
+            //cropBitmap1 = croppedBitmap;
+            croppedImageIv.setImageBitmap(croppedBitmap);
+            img1 = false;
+            //return croppedBitmap;
+        } else {
+            //cropBitmap2 = croppedBitmap;
+            originalImageIv.setImageBitmap(croppedBitmap);
+            img1 = true;
         }
-        Toast.makeText(this, "" + totalObservations, Toast.LENGTH_SHORT).show();
+
+//            int[] histogram2 = histogramComputing.HistogramComputing(cropBitmap2);
+//            int totalObservations1 = 0, totalObservations2 = 0;
+//            for (int i = 0; i < histogram1.length; i++) {
+//                totalObservations1 += histogram1[i];
+//                totalObservations2 += histogram2[i];
+//            }
+//            Toast.makeText(this, "" + totalObservations2, Toast.LENGTH_SHORT).show();
+//            double[] expectedFrequencies1 = new double[histogram1.length];
+//            double[] expectedFrequencies2 = new double[histogram2.length];
+//
+//            for (int i = 0; i < histogram1.length; i++) {
+//                expectedFrequencies1[i] = (double) totalObservations1 * (histogram1[i] + histogram2[i]) / (double) (totalObservations1 + totalObservations2);
+//                expectedFrequencies2[i] = (double) totalObservations2 * (histogram2[i] + histogram1[i]) / (double) (totalObservations1 + totalObservations2);
+//            }
+//
+//            double chiSquare = 0.0;
+//            //Log.d("fffF", String.valueOf(chiSquare));
+//
+//            for (int i = 0; i < histogram1.length; i++) {
+//                double diff1 = histogram1[i] - expectedFrequencies1[i];
+//                double diff2 = histogram2[i] - expectedFrequencies2[i];
+//
+//                if (expectedFrequencies1[i] > 0)
+//                    chiSquare += (diff1 * diff1) / expectedFrequencies1[i];
+//                if (expectedFrequencies2[i] > 0)
+//                    chiSquare += (diff2 * diff2) / expectedFrequencies2[i];
+//            }
+//
+//            TextView NTN = findViewById(R.id.NTN);
+//
+//            NTN.setText(String.format("%.2f", chiSquare));
+//            img1 = true;
+//            ///Toast.makeText(this, "" + chiSquare, Toast.LENGTH_SHORT).show();
+//            img1 = true;
+        //Bitmap bitmap1 = BitmapFactory.decodeResource(getResources(), R.drawable.thay_trinh_2);
+//        if (!img1) {
+//            HistogramComputing histogramComputing = new HistogramComputing();
+//            int[] histogram1 = histogramComputing.HistogramComputing(cropBitmap1);
+//            int[] histogram2 = histogramComputing.HistogramComputing(cropBitmap2);
+//            int totalObservations1 = 0, totalObservations2 = 0;
+//            for (int i = 0; i < histogram1.length; i++) {
+//                totalObservations1 += histogram1[i];
+//                totalObservations2 += histogram2[i];
+//            }
+//            Toast.makeText(this, "" + totalObservations2, Toast.LENGTH_SHORT).show();
+//            double[] expectedFrequencies1 = new double[histogram1.length];
+//            double[] expectedFrequencies2 = new double[histogram2.length];
+//
+//            for (int i = 0; i < histogram1.length; i++) {
+//                expectedFrequencies1[i] = (double) totalObservations1 * (histogram1[i] + histogram2[i]) / (double) (totalObservations1 + totalObservations2);
+//                expectedFrequencies2[i] = (double) totalObservations2 * (histogram2[i] + histogram1[i]) / (double) (totalObservations1 + totalObservations2);
+//            }
+//
+//            double chiSquare = 0.0;
+//            //Log.d("fffF", String.valueOf(chiSquare));
+//
+//            for (int i = 0; i < histogram1.length; i++) {
+//                double diff1 = histogram1[i] - expectedFrequencies1[i];
+//                double diff2 = histogram2[i] - expectedFrequencies2[i];
+//
+//                if (expectedFrequencies1[i] > 0)
+//                    chiSquare += (diff1 * diff1) / expectedFrequencies1[i];
+//                if (expectedFrequencies2[i] > 0)
+//                    chiSquare += (diff2 * diff2) / expectedFrequencies2[i];
+//            }
+//
+//            TextView NTN = findViewById(R.id.NTN);
+//
+//            NTN.setText(String.format("%.2f", chiSquare));
+//            img1 = true;
+//            ///Toast.makeText(this, "" + chiSquare, Toast.LENGTH_SHORT).show();
+
+//        }
+
+    }
+
+    private void HistogramCalculate(Bitmap cropBitmap1, Bitmap cropBitmap2) {
+
+        HistogramComputing histogramComputing = new HistogramComputing();
+        int[] histogram1 = histogramComputing.HistogramComputing(cropBitmap1);
+        int[] histogram2 = histogramComputing.HistogramComputing(cropBitmap2);
+        int totalObservations1 = 0, totalObservations2 = 0;
+        for (int i = 0; i < histogram1.length; i++) {
+            totalObservations1 += histogram1[i];
+            totalObservations2 += histogram2[i];
+        }
+        Toast.makeText(this, "" + totalObservations2, Toast.LENGTH_SHORT).show();
         double[] expectedFrequencies1 = new double[histogram1.length];
         double[] expectedFrequencies2 = new double[histogram2.length];
 
         for (int i = 0; i < histogram1.length; i++) {
-            expectedFrequencies1[i] = (double) totalObservations * histogram1[i] / (double) totalObservations;
-            expectedFrequencies2[i] = (double) totalObservations * histogram2[i] / (double) totalObservations;
+            expectedFrequencies1[i] = (double) totalObservations1 * (histogram1[i] + histogram2[i]) / (double) (totalObservations1 + totalObservations2);
+            expectedFrequencies2[i] = (double) totalObservations2 * (histogram2[i] + histogram1[i]) / (double) (totalObservations1 + totalObservations2);
         }
-
-        double chiSquare = 0.0;
+        //Log.d("fffF", String.valueOf(chiSquare));
 
         for (int i = 0; i < histogram1.length; i++) {
             double diff1 = histogram1[i] - expectedFrequencies1[i];
             double diff2 = histogram2[i] - expectedFrequencies2[i];
 
-            chiSquare += (diff1 * diff1) / expectedFrequencies1[i];
-            chiSquare += (diff2 * diff2) / expectedFrequencies2[i];
+            if (expectedFrequencies1[i] > 0)
+                chiSquare += (diff1 * diff1) / expectedFrequencies1[i];
+            if (expectedFrequencies2[i] > 0)
+                chiSquare += (diff2 * diff2) / expectedFrequencies2[i];
         }
 
         TextView NTN = findViewById(R.id.NTN);
-        //NTN.setText(String.format("%.2f", chiSquare));
-        ///Toast.makeText(this, "" + chiSquare, Toast.LENGTH_SHORT).show();
 
+        NTN.setText(String.format("%.2f", chiSquare));
+        ///Toast.makeText(this, "" + chiSquare, Toast.LENGTH_SHORT).show();
     }
 }
