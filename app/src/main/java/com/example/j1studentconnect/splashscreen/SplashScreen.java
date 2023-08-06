c260908abad8b8563413efb64f6900f8a4430dba
ngoctuannguyen <ngoctuannguyen1980123@gmail.com>
2023-08-06 16:17:04 +0700

    Layout Des

--- app/src/main/java/com/example/j1studentconnect/splashscreen/SplashScreen.java
+++ app/src/main/java/com/example/j1studentconnect/splashscreen/SplashScreen.java
@@ -2,15 +2,30 @@ package com.example.j1studentconnect.splashscreen;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
 
 import com.example.j1studentconnect.R;
+import com.example.j1studentconnect.authentication.LoginLauncher;
 
 public class SplashScreen extends AppCompatActivity {
+
+    private static final long SPLASH_DISPLAY_TIME = 300;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_splash_screen);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent mainIntent = new Intent(SplashScreen.this, LoginLauncher.class);
+                startActivity(mainIntent);
+                finish();
+            }
+        }, SPLASH_DISPLAY_TIME);
     }
 }
